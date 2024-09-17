5895e43d6b68e6fbd75f3922c66269fa1785ce60
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-09-17 15:55:41 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ go get github.com/caarlos0/env/v11
 go get github.com/joho/godotenv
 go get github.com/go-playground/validator/v10
 go get github.com/jackc/pgx/v5/stdlib
+go get github.com/stretchr/testify/assert
